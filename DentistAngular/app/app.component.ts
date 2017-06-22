c2f56fc91203fc15356d1f2e8335c2eeafd1e919
leangele <cardonaangelo@hotmail.com>
2017-06-22 12:30:16 -0400

    add angular project

---
+++ DentistAngular/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+import { ContactFormComponent} from './contact-form.component';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular App</h1>'
+})
+export class AppComponent { }
